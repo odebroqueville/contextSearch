7fdd8992eff041f2473f6ef8bd99a72e78ea480e
Olivier de Broqueville <olivier.debroqueville@gmail.com>
2020-05-10 13:15:15 +0200

    turn console logging ogg

--- scripts/options.js
+++ scripts/options.js
@@ -1,7 +1,7 @@
 /// Global variables
 /* global sortByIndex, Sortable */
 // Debugging
-const logToConsole = true;
+const logToConsole = false;
 
 // Settings container and div for addSearchEngine
 const divContainer = document.getElementById('container');
