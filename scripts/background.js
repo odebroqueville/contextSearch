adb4c677a9ef8ddb0305e993fbbf188c8f0becf7
Olivier de Broqueville <olivier.debroqueville@gmail.com>
2020-09-30 22:15:13 +0200

    bug fix

--- scripts/background.js
+++ scripts/background.js
@@ -474,6 +474,9 @@ function initialiseSearchEngines(forceReload) {
 										}
 										reject();
 									});
+							} else {
+								rebuildContextMenu();
+								resolve();
 							}
 						})
 						.catch((err) => {
